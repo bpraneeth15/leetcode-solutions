82e16a1e247980a597f0fa3354c717abb6bc7219
bpraneeth15 <99836323+bpraneeth15@users.noreply.github.com>
2026-01-25 23:54:09 -0600

    Added min cost of chips solution

--- src/main/java/org/example/Arrays/ArrEasy/MinCostChips.java
+++ src/main/java/org/example/Arrays/ArrEasy/MinCostChips.java
@@ -2,5 +2,30 @@ package org.example.Arrays.ArrEasy;
 
 public class MinCostChips {
 
+    public static void main(String[] args) {
 
+        System.out.println(minCostToMoveChips(new int[]{1, 2, 3}));
+    }
+
+    public static int minCostToMoveChips(int[] position) {
+
+
+        int evenNumbers = 0;
+        int oddNumbers = 0;
+        for (int i = 0; i <= position.length - 1; i++) {
+
+            //count the no of even and odd positions return the value of lesser couont
+            if (position[i] % 2 == 0) {
+                //even'
+                evenNumbers++;
+            } else {
+                oddNumbers++;
+            }
+        }
+        return oddNumbers < evenNumbers ? oddNumbers:evenNumbers;
+    }
 }
+
+
+//Question explanation:
+//
